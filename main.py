57ec98a723567879df73550f57182e242cddf5c8
Phil Wilko <philwilkinson40@gmail.com>
2018-11-18 10:10:31 +0800

    _

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import dht12
 import machine
 
 i2c = machine.I2C(sda = machine.Pin(4), scl = machine.Pin(5))
+
+'''
+confirm your device is present on the bus
+it should return [92] for this particular sensor
+if this returns nothing, check your wiring
+then check the Pin GPIO numbers are correct 
+'''
+
+print(i2c.scan())
+
 sensor = dht12.DHT12(i2c)
 
 sensor.measure()
